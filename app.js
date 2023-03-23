0c7f2f195c4e1405a19e964ff44af02377253075
Artem <artic12101997dr@gmail.com>
2023-03-23 16:07:29 +0200

    init .env and mongoose

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const { DB_HOST } = process.env;
+
+mongoose
+    .connect(DB_HOST)
+    .then(() => console.log("database connect"))
+    .catch((err) => console.log(err.message));
 
 const contactsRouter = require("./routes/api/contacts");
 
